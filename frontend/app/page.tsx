ff656ada5be59fed7c640fb8768598d3777deadc
Phamela Mhlaba <phamelamhlaba@gmail.com>
2025-08-18 15:37:11 +0200

    implemented the client success stories into my homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -138,6 +138,43 @@ export default function HomePage ()  {
         <button>View ALL Services</button>
       </section>
 
+      {/*Client Success Stories Section*/}
+
+      <section>
+        <h2>
+          Client Success stories
+        </h2>
+
+      {/* Client Review 1*/}
+        <div>
+          <div>★★★★★</div>
+          <p>"The AI Chatbot transformed our customer service. Responsive time decreased by 80% and customer satisfication increased significantly"</p>
+          <p>Sarah Johnson</p>
+          <p>CTO, GrowthCorp</p>
+        </div>
+
+      {/* Client Review 1*/}
+        <div>
+          <div>★★★★★</div>
+          <p>"Outsatnding work on our web application. The scalable arhcitecture handles our growing user base perfectly."</p>
+          <p>Micheal chen</p>
+          <p>CTO, GrowthCopr</p>
+        </div>
+
+      {/* Client Review 3 */}
+        <div>
+          <div>★★★★★</div>
+          <p>"The predictive analytucs solutions increased our sales forecast accuracy by 40%.Incredible ROI"</p>
+          <p>Emma Rodrigues</p>
+          <p>VP Sales, DataDriven LLC</p>
+        </div>
+
+      {/* Button that render a contact from for more enquiries */}
+        <div>
+          <button>Start Your Project</button>
+        </div>
+      </section>
+
     </div>
-  )
-}
+  );
+};
