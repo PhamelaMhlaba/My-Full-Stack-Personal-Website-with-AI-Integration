f1afc56e9c421c9cbc66fccb45b6c499cfaf71d0
Phamela Mhlaba <phamelamhlaba@gmail.com>
2025-12-24 19:11:50 +0200

    Implemented the button for View All Post of the latest Insights blog post section

--- frontend/app/components/LatestInsights.tsx
+++ frontend/app/components/LatestInsights.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import  Posts  from "../data/posts";
 import styles from  "./LatestInsights.module.css";
 import BlogPost from "./BlogPost";
+import Link from "next/link";
 
 export default function LatestInsights() {
     return (
@@ -21,6 +22,11 @@ export default function LatestInsights() {
                 {Posts.map((post) => (
                  <BlogPost key={post.id} post={post} />
                 ))} 
+            </div>
+            <div className={styles.buttonContainer}>
+                    <Link href="/tech-blog" className={styles.viewAllButtonLink}>
+                        View all Posts
+                    </Link>
             </div>
         </section>
     )
